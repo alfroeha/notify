bd1812a0ca617c18bfe8dbd17edd8dd9a9e49a2f
Alf <edelweissbrekker@gmail.com>
2023-05-26 22:25:55 +0700

    Update models.py

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.conf import settings
 # Create your models here.
+
+class Course(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE,
+                              related_name='courses', 
+                              related_query_name='course')
+    name=models.CharField(max_length=250)
+    day=models.CharField(max_length=20)
+    room=models.CharField(max_length=20)
+
